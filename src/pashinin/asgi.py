46d5efb7c9c86ddea94b7795c0b1e2e4213693d0
Sergey Pashinin <sergey@pashinin.com>
2016-10-22 20:27:04 +0300

    asgi

---
+++ src/pashinin/asgi.py
@@ -0,0 +1,6 @@
+import os
+import channels.asgi
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pashinin.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pashinin.settings')
+channel_layer = channels.asgi.get_channel_layer()
