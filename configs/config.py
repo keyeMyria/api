0ed1b66d99c34eb7a1a7ac38c5b0349e38a4f4cd
Sergey Pashinin <sergey@pashinin.com>
2016-10-25 14:17:56 +0300

    generate vars scripts

---
+++ configs/config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import argparse
+import json
+
+# This program adds some variables to a supplied config file.
+output = '/tmp/conf.json'
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Base config file (JSON)')
+    args = parser.parse_args()
+
+    data = {}
+    with open(args.file) as f:
+        data = json.load(f)
+
+    data['vebin'] = os.path.dirname(sys.executable)
+    if (sys.version_info >= (3, 5)):
+        # print(sys.executable)
+        pass
+    else:
+        # print('create ve')
+        pass
+
+    with open(output, 'w') as f:
+        json.dump(data, f)
+
+    print(output, end="")
