e5aaa397298a4ff38d06ca9657c06bfc4b7ff551
Sergey Pashinin <sergey@pashinin.com>
2016-11-04 17:01:12 +0300

    templates changes

--- configs/dbinit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# until psql -h db -U "postgres" -c '\l'; do
-#     >&2 echo "Postgres is unavailable - sleeping"
-#     sleep 1
-# done
-# sleep 1
-psql -h db -U postgres -c "CREATE USER pashinin WITH PASSWORD 'superpass';"
-psql -h db -U postgres -c "CREATE DATABASE pashinin WITH OWNER pashinin;"
-# psql -h db -U postgres -c "grant all privileges on database pashinin to pashinin;"
