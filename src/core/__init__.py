6119fbd82afcfe69b9fbac13d454f423f470163c
Sergey Pashinin <sergey@pashinin.com>
2016-12-21 02:34:49 +0300

    update celery version

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -44,6 +44,14 @@ def get_client_ip(request):
 
 
 def travis(func):
+    """Decorator to run a function only if we are in Travis.
+
+    @travis
+    def func1():
+        pass
+
+    func1()  # will run only if in Travis build
+    """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         res = None
