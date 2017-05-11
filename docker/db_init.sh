10070c437ae36f1eb3388133d0eb2d5b9f9075e7
Sergey Pashinin <sergey@pashinin.com>
2017-05-11 13:36:55 +0300

    docker upd

--- docker/db_init.sh
+++ docker/db_init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-(cd /var/www/project/src; ./manage.py migrate --run-syncdb --settings=pashinin.settings)
-(cd /var/www/project/src; ./manage.py migrate --settings=pashinin.settings)
-
-#
+(cd /var/www/project/src; python manage.py migrate --run-syncdb --settings=pashinin.settings)
+(cd /var/www/project/src; python manage.py migrate --settings=pashinin.settings)
+(cd /var/www/project/src; python manage.py loaddata --settings=pashinin.settings initial_data.json articles_examples.json ege_subjects.json)
