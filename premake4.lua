0793b2455d929750b056386769cb0007d3a634db
Sergey Pashinin <sergey@pashinin.com>
2017-12-14 14:03:23 +0300

    some win stuff

--- premake4.lua
+++ premake4.lua
@@ -1,11 +1,15 @@
 -- https://nodejs.org/dist/v7.10.0/node-v7.10.0-x64.msi
 
---os.execute("notepad.exe")
---os.execute("docker container ls")
+e = os.execute("python -V")
+if e == 0 then
+  print("Python already installed. OK")
+else
+  print("Installing Python...")
+end
 
---(cd configs; make templates)
-os.execute("docker-compose -f docker/docker-compose.yml run --rm --workdir=/var/www/project/configs django make templates")
-os.execute("docker-compose -f docker/docker-compose.yml up")
+e = os.execute("python -m src.core.win.setup")
+--os.execute("docker-compose -f docker/docker-compose.yml run --rm --workdir=/var/www/project/configs django make templates")
+--os.execute("docker-compose -f docker/docker-compose.yml up")
 
 
 print("OK.DONE.")
